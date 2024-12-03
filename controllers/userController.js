47deb1953a56285d5a9c124816f7c3498b85efc8
Soumya Annie Thomas <soumyaannie07@gmail.com>
2024-12-03 07:12:12 +0530

    first commit

---
+++ controllers/userController.js
@@ -0,0 +1,12 @@
+//register
+exports.registerController = (req, res) => {
+    console.log("Inside regosterController");
+    const { username, email, password } = req.body
+    console.log(username, email, password);
+    
+    res.status(200).json("Request received!!!")
+}
+
+//login
+
+//Profile updation
